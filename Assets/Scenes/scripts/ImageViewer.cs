8ed506af2268d8d61ca26ad81b93d4beb44becda
Juan Jose Garcia <juanjosegarciacan@gmail.com>
2024-09-20 11:56:30 +0200

    Adds image viewer dynamic load

--- Assets/Scenes/scripts/ImageViewer.cs
+++ Assets/Scenes/scripts/ImageViewer.cs
@@ -4,6 +4,9 @@ using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Video;
+using System.Threading.Tasks;
+
+
 public class ImageViewer : MonoBehaviour
 {
 
@@ -12,8 +15,10 @@ public class ImageViewer : MonoBehaviour
     public GameObject nextViewer;
 
     
-    private List<Texture> images = new List<Texture>();
-    private List<VideoPlayer> players = new List<VideoPlayer>();
+    private List<string> imagesURLs = new List<string>();
+    private List<bool> imagesLoaded = new List<bool>();
+    private List<Texture2D> images = new List<Texture2D>();
+    
     public int offset = 0;
     private int currentImg = 0;
     private int l = 0;
@@ -21,9 +26,16 @@ public class ImageViewer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        loadImages();
+        loadURLs();
+        loadURLs("png");
+        l = imagesURLs.Count;
+        Debug.Log($"Number of images: {l}");
+        for ( int i = 0; i < l; ++i)
+        {
+            imagesLoaded.Add(false);
+            images.Add(new Texture2D(2,2)); 
+        }
 
-        l = images.Count;
         currentImg = clamp(offset);
         setImage(mainViewer, currentImg);       
         setImage(prevViewer, currentImg-1);     
@@ -36,48 +48,55 @@ public class ImageViewer : MonoBehaviour
         
     }
 
-    private void loadImages(string extension = "jpg")
+    private void loadImage(int index)
+    {
+        byte[] bytes = File.ReadAllBytes(imagesURLs[index]);
+        images[index].LoadImage(bytes);
+        // images[index].Compress(true);
+        imagesLoaded[index] = true;
+    }
+    private void unloadImage(int index)
+    {
+    
+        Object.Destroy (images[index]);
+        images[index] = new Texture2D(2,2);
+        imagesLoaded[index] = false;
+    }
+
+    private void loadURLs(string extension = "jpg")
     {
         var path = Application.persistentDataPath + Path.DirectorySeparatorChar;
         var paths = Directory.EnumerateFiles(path, $"*.{extension}");
         foreach (var p in paths)
-        {
-            byte[] pngBytes = File.ReadAllBytes(p);
-            Texture2D tex = new Texture2D(2,2);
-            tex.LoadImage(pngBytes);
-            images.Add(tex);
-        }
+            imagesURLs.Add(p);
     }
 
     private int clamp(int id)
     {
         int newId = id;
          if (newId < 0)
-            newId = l - 1;
+            newId += l;
          if (newId > l - 1)
-            newId = 0;
+            newId -= l;
         return newId;
     }
 
-    void setImage(GameObject viewer, int id)
+    private void setImage(GameObject viewer, int id)
     {
         id = clamp(id);
+        if (!imagesLoaded[id])
+            loadImage(id);
 
         RawImage rawImage = viewer.GetComponent<RawImage>();
         RectTransform rec = viewer.GetComponent<RectTransform>();
-
         rawImage.texture = images[id];    
-        
         float ratio = (float)images[id].width / images[id].height; 
-
         float x = 1.0f;
         float y = 1.0f;
-        
         if (ratio >= 1.0 )
             y = 1.0f/ratio;
         else
             x = ratio;
-
         rec.localScale = new Vector3(x, y, 1);
     }
 
@@ -88,6 +107,9 @@ public class ImageViewer : MonoBehaviour
         setImage(mainViewer, currentImg);
         setImage(prevViewer, currentImg-1);     
         setImage(nextViewer, currentImg+1);
+        
+        int uIndex = clamp(currentImg-2);
+        unloadImage(uIndex);
     }
 
     public void previousImage()
@@ -97,6 +119,9 @@ public class ImageViewer : MonoBehaviour
         setImage(mainViewer, currentImg);        
         setImage(prevViewer, currentImg-1);     
         setImage(nextViewer, currentImg+1);
+        
+        int uIndex = clamp(currentImg+2);
+        unloadImage(uIndex);
     }
 
 }
